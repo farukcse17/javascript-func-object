d14d4b8ceeecf329598bd3cf2a5f1dc3f6bc161f
unknown <faruk.jsor@gmail.com>
2023-08-02 10:53:07 +0600

    added problem1.js

---
+++ problem1.js
@@ -0,0 +1,12 @@
+//Write a function called foo() which prints "foo" and a funciton called bar() which prints "bar". Call function bar() in the foo() function after printing. What will be the output? Now call the foo() to see the output.
+
+// Foo function
+function foo(){
+    console.log("foo");
+}
+foo();
+// Bar Function
+function bar(){
+    console.log("bar");
+}
+bar()
